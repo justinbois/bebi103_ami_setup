2f9a2f43ec1cccf928c7a9b42b0379cf46ee424f
justinbois <bois@caltech.edu>
2020-01-20 16:13:21 -0800

    Setup used for initial setup for 2020 b

--- setup_ami.sh
+++ setup_ami.sh
@@ -13,7 +13,7 @@ source ~/.bashrc;
 # Install packages
 conda update -q -y conda;
 conda install -y numpy cython;
-conda install -y scipy pandas numba scikit-image scikit-learn statsmodels bokeh tqdm matplotlib seaborn ipython xlrd jupyter jupyterlab black nodejs xarray netcdf4 ujson hypothesis;
+conda install -y scipy pandas numba scikit-image scikit-learn statsmodels bokeh tqdm matplotlib seaborn ipython xlrd jupyter jupyterlab black nodejs xarray netcdf4 ujson h5py hypothesis;
 
 # Uncomment to install sphinx packages for building course
 # pip install recommonmark commonmark nbsphinx sphinx-rtd-theme
@@ -37,6 +37,9 @@ jupyter labextension install --no-build @ijmbarr/jupyterlab_spellchecker;
 jupyter labextension install --no-build @pyviz/jupyterlab_pyviz;
 jupyter lab build;
 
+# Clone the updater
+git clone https://github.com/justinbois/bebi103_update.git;
+
 # Configure bash
 echo 'export PS1="\[\e[1;32m\]\u\[\e[0m\]@\e[1;36m\]\h\[\e[0m\] [\w]\n% "' >> ~/.bashrc;
 echo 'alias rm="rm -i"' >> ~/.bashrc;
@@ -44,8 +47,12 @@ echo 'alias mv="mv -i"' >> ~/.bashrc;
 echo 'alias cp="cp -i"' >> ~/.bashrc;
 echo 'alias ls="ls -FGh"' >> ~/.bashrc;
 echo 'export LSCOLORS="gxfxcxdxCxegedabagacad"' >> ~/.bashrc;
+echo 'alias bebi103_update="~/bebi103_update/bebi103_update.sh"' >> ~/.bashrc;
 
 # Get the data sets
 wget https://s3.amazonaws.com/bebi103.caltech.edu/2020b/data.tar;
 tar -xvf data.tar;
-rm ./data.tar;
+rm -f ./data.tar;
+
+# Clean up miniconda
+rm -f miniconda.sh;
